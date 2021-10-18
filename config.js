6826e2a74c590247d6429e054b261157ee8d32b2
ben-larcombe <62991614+ben-larcombe@users.noreply.github.com>
2021-10-18 23:05:59 +1100

    Working V1

--- config.js
+++ config.js
@@ -8,23 +8,24 @@ config.certificateConfig = {
 config.sessionSecret='uwotm8';
 
 //Port you want Node.js to listen to for authentication requests. 
-config.port='8084';
+config.port='443';
 
 //Example RESTURI for API Endpoint. Do not forget to include virtual proxy in path.
 //default: config.RESTURI='https://servername.com:4243/qps/custom';
-config.RESTURI='https://qlikserver.domain.com.com:4243/qps/custom';
+config.RESTURI='https://GFAU-NDC-LTA01:4243/qps/ltena_test';
 
 //If you're using the login page as the "landing page" instead of redirecting from a virtual proxy
 // - make sure you set up this URI to redirect the user after authenticating and receiving a ticket 
-config.REDIRECT='https://qlikserver.domain.com/custom/hub';
+// config.REDIRECT='https://GFAU-NDC-LTA01/custom/hub';
+config.REDIRECT='https://gfau-ndc-lta01/sense/app/aefabd8d-a1d1-4f46-84cf-1b02206f79d1/overview'
 
 //Configure the path for the LDAP server you want to authenticate the user against
-config.LDAP='ldap://ldapserver.domain.com:389/';
+config.LDAP='ldap://gfau.gfiroot.local:389/';
 
 //This is appended to the username that the user provides in the form. Example: user keys in 'UserX' and the config.DOMAIN part appends '@domain.com'
-config.DOMAIN = '@domain.com';
+config.DOMAIN = '@gfau.gfiroot.local';
 
 //Specify the name of the user directory you want to use for the Qlik Sense ticket requests
-config.USERDIRECTORY = 'USERDIRECTORYNAME';
+config.USERDIRECTORY = 'GFAU';
 
 module.exports = config;
